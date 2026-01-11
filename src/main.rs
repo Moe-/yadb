2f48f63d2c6e412442d30ff1aae74de9859f3778
Markus <moe@Moe-Laptop.localdomain>
2026-01-11 22:55:26 +0100

    Add attach capability

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 pub mod yadb;
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use nix::sys::wait;
+use nix::unistd::Pid;
 
-    yadb::attach::attach();
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let pid = yadb::attach::attach(&args).unwrap();
+
+    println!("Process PID is {pid}");
+
+    match wait::waitpid(Pid::from_raw(pid), Option::None) {
+        Err(err) => println!("waitpid failed {err}"),
+        Ok(_) => {},
+    }
+
+    // implement waitpid, see https://docs.rs/fork/latest/fork/
 }
+
--- src/yadb/attach.rs
+++ src/yadb/attach.rs
@@ -1,3 +1,31 @@
-pub fn attach() {
-    println!("Test!");
+use nix::sys::ptrace;
+use nix::unistd::Pid;
+use std::process::Command;
+
+pub fn attach(args: &Vec<String>) -> Result<i32, &str> {
+    let pid;
+    if args.len() == 3 && args[1] == "-p" {
+        pid = args[2].parse::<i32>().unwrap();
+        if pid <= 0 {
+            return Err("Invalid PID");
+        }
+        
+    } else {
+       let mut new_args = args.clone();
+       new_args = new_args.drain(2..).collect();
+       let child = Command::new(&args[1])
+                .args(new_args)
+                .spawn()
+                .expect("failed to execute process");
+        pid = child.id() as i32;
+    }
+
+    println!("Trying to attach to PID {pid}");
+    return match ptrace::attach(Pid::from_raw(pid)) {
+        Err(err) => {
+            println!("Failed to attach with error {err}");
+            return Err("Failed to attach");
+        },
+        Ok(_) => Ok(pid)
+    };
 }
