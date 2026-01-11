da696b8bdd053923ea34166eecf6fc5392b3a3ff
Markus <moe@Moe-Laptop.localdomain>
2026-01-11 23:52:55 +0100

    simple command line interface

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ pub mod yadb;
 use std::env;
 use nix::sys::wait;
 use nix::unistd::Pid;
+use std::io;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,6 +17,30 @@ fn main() {
         Ok(_) => {},
     }
 
-    // implement waitpid, see https://docs.rs/fork/latest/fork/
+    let mut buffer = String::new();
+    let mut history = Vec::<String>::new();
+    let stdin = io::stdin();
+    while let result = stdin.read_line(&mut buffer) {
+        match result {
+            Ok(n) => {
+                println!("read {n} characters: {buffer}");
+                match n {
+                    0 => break,
+                    1 => { // use last history entry
+                        history.last().map(|it|handle_command(pid, &it));
+                    },
+                    _ => { // use new entry
+                        history.push(buffer.clone());
+                        handle_command(pid, &buffer);
+                    },
+                }
+                buffer.clear();
+            },
+            Err(err) => println!("read_line failed {err}"),
+        }
+    }
 }
 
+fn handle_command(pid: i32, command: &String) {
+    println!("Handle command: {pid} - {command}");
+}
